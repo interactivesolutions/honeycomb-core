256e88f90fa1a39d0959c4c6eb0ee718bf2cf43d
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-12-29 13:18:44 +0200

    HCCommand file update
    - upgraded createDirectory function
    - added createFileFromTemplate function

--- src/commands/HCCommand.php
+++ src/commands/HCCommand.php
@@ -3,9 +3,28 @@
 namespace interactivesolutions\honeycombcore\commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 
 class HCCommand extends Command
 {
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * Create a new controller creator command instance.
+     *
+     * @param \Illuminate\Filesystem\Filesystem|Filesystem $files
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+        $this->files = $files;
+    }
+
     /**
      * Creating directory if not exists
      *
@@ -13,8 +32,23 @@ class HCCommand extends Command
      */
     public function createDirectory($path)
     {
-        if (!file_exists($path))
-            mkdir($path);
+        $path = str_replace('\\', '/', $path);
+        $path = explode('/', $path);
+
+        $finalDirectory = '';
+
+        foreach($path as $directory)
+        {
+            $finalDirectory .= $directory;
+
+            if (!file_exists($finalDirectory))
+            {
+                print_r($finalDirectory . ' created');
+                mkdir($finalDirectory);
+            }
+
+            $finalDirectory .= '/';
+        }
     }
 
     /**
@@ -39,5 +73,32 @@ class HCCommand extends Command
             shell_exec('rm' . $withFiles . $path);
         else
             $this->info($path . ' directory does not exists');
+    }
+
+    /**
+     * Replace file
+     * @param $configuration
+     * @internal param $destination
+     * @internal param $templateDestination
+     * @internal param array $content
+     */
+    public function createFileFromTemplate($configuration)
+    {
+        $destination = $configuration['destination'];
+        $templateDestination = $configuration['templateDestination'];
+
+        if (!isset($destination))
+            $this->error('File creation failed, destination not set');
+
+        if (!isset($templateDestination))
+            $this->error('File creation failed, template destination not set');
+
+        $destination = str_replace('\\', '/', $destination);
+
+        $template = $this->files->get($templateDestination);
+        $template = replaceBrackets($template, $configuration['content']);
+
+        $this->files->put($destination, $template);
+
     }
 }
