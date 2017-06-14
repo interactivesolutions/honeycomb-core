fea2c2616a0c38fd6160ff1412d7f7055afd240c
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-06-14 21:44:46 +0300

    Added hcview function

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -230,3 +230,22 @@ if( ! function_exists('createTranslationKey') ) {
         return str_replace(' ', '_', strtolower($string));
     }
 }
+
+if( ! function_exists('hcview') ) {
+    /**
+     * Get the evaluated view contents for the given view.
+     *
+     * @param  string $view
+     * @param  array $data
+     * @param  array $mergeData
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     */
+    function hcview($view = null, $data = [], $mergeData = [])
+    {
+        if( ! array_key_exists($view, config('hc.views')) ) {
+            return view($view, $data, $mergeData);
+        }
+
+        return view(array_get(config('hc.views'), $view), $data, $mergeData);
+    }
+}
