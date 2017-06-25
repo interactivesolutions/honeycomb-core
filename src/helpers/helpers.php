84d1116a5b8e32577c3d5d6b01a6795c56b86478
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-06-25 13:04:06 +0300

    Addded hcSuccess function

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-if( ! function_exists('get_translation_name') ) {
+if (!function_exists ('get_translation_name')) {
     /**
      * Get translation name from
      *
@@ -9,47 +9,47 @@ if( ! function_exists('get_translation_name') ) {
      * @param array $data
      * @return mixed
      */
-    function get_translation_name(string $key, string $lang, array $data)
+    function get_translation_name (string $key, string $lang, array $data)
     {
-        if( is_array($data) ) {
-            $data = collect($data);
+        if (is_array ($data)) {
+            $data = collect ($data);
         }
 
-        $item = $data->where('language_code', $lang)->first();
+        $item = $data->where ('language_code', $lang)->first ();
 
-        if( is_null($item) ) {
-            $name = array_get($data, '0.' . $key);
+        if (is_null ($item)) {
+            $name = array_get ($data, '0.' . $key);
         } else {
-            $name = array_get($item, $key);
+            $name = array_get ($item, $key);
         }
 
-        if( is_null($name) ) {
-            $name = trans('core::core.no_translation');
+        if (is_null ($name)) {
+            $name = trans ('core::core.no_translation');
         }
 
         return $name;
     }
 }
 
-if( ! function_exists('uuid4') ) {
+if (!function_exists ('uuid4')) {
     /**
      * Generates uuid4 id
      *
      * @param bool $toString
      * @return \Ramsey\Uuid\UuidInterface|string
      */
-    function uuid4(bool $toString = false)
+    function uuid4 (bool $toString = false)
     {
-        $uuid4 = Ramsey\Uuid\Uuid::uuid4();
+        $uuid4 = Ramsey\Uuid\Uuid::uuid4 ();
 
-        if( $toString )
-            $uuid4 = $uuid4->toString();
+        if ($toString)
+            $uuid4 = $uuid4->toString ();
 
         return $uuid4;
     }
 }
 
-if( ! function_exists('pluralizeLT') ) {
+if (!function_exists ('pluralizeLT')) {
     /**
      * Returns the correct lithuanian word form for given count.
      *
@@ -59,19 +59,19 @@ if( ! function_exists('pluralizeLT') ) {
      * @throws \InvalidArgumentException
      * @return string
      */
-    function pluralizeLT(array $words, int $n)
+    function pluralizeLT (array $words, int $n)
     {
-        if( count($words) != 3 ) {
+        if (count ($words) != 3) {
             throw new \InvalidArgumentException("Words array must contain 3 values!");
         }
 
-        if( ! is_int($n) ) {
+        if (!is_int ($n)) {
             throw new \InvalidArgumentException("n must be an integer!");
         }
 
-        if( $n % 10 == 0 || floor($n / 10) == 1 ) {
+        if ($n % 10 == 0 || floor ($n / 10) == 1) {
             return $words[2];
-        } elseif( $n % 10 == 1 ) {
+        } elseif ($n % 10 == 1) {
             return $words[0];
         } else {
             return $words[1];
@@ -79,30 +79,30 @@ if( ! function_exists('pluralizeLT') ) {
     }
 }
 
-if( ! function_exists('isPackageEnabled') ) {
+if (!function_exists ('isPackageEnabled')) {
     /**
      * Check if package is registered at config/app.php file
      *
      * @param $provider
      * @return bool
      */
-    function isPackageEnabled($provider)
+    function isPackageEnabled ($provider)
     {
-        $registeredProvidersArray = array_keys(app()->getLoadedProviders());
+        $registeredProvidersArray = array_keys (app ()->getLoadedProviders ());
 
-        return in_array($provider, $registeredProvidersArray);
+        return in_array ($provider, $registeredProvidersArray);
     }
 }
 
-if( ! function_exists('settings') ) {
+if (!function_exists ('settings')) {
     //TODO create settings service
-    function settings($key)
+    function settings ($key)
     {
         return $key;
     }
 }
 
-if( ! function_exists('sanitizeString') ) {
+if (!function_exists ('sanitizeString')) {
 
     /**
      * Returns a sanitized string, typically for URLs.
@@ -114,7 +114,7 @@ if( ! function_exists('sanitizeString') ) {
      *
      * @return mixed|string
      */
-    function sanitizeString(string $string, bool $forceLowerCase = false, bool $onlyLetter = false)
+    function sanitizeString (string $string, bool $forceLowerCase = false, bool $onlyLetter = false)
     {
         $strip = [
             "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
@@ -122,19 +122,19 @@ if( ! function_exists('sanitizeString') ) {
             "â€”", "â€“", ",", "<", ".", ">", "/", "?",
         ];
 
-        $clean = trim(str_replace($strip, "", strip_tags($string)));
-        $clean = preg_replace('/\s+/', "-", $clean);
-        $clean = ($onlyLetter) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean;
+        $clean = trim (str_replace ($strip, "", strip_tags ($string)));
+        $clean = preg_replace ('/\s+/', "-", $clean);
+        $clean = ($onlyLetter) ? preg_replace ("/[^a-zA-Z0-9]/", "", $clean) : $clean;
 
         return ($forceLowerCase) ?
-            (function_exists('mb_strtolower')) ?
-                mb_strtolower($clean, 'UTF-8') :
-                strtolower($clean) :
+            (function_exists ('mb_strtolower')) ?
+                mb_strtolower ($clean, 'UTF-8') :
+                strtolower ($clean) :
             $clean;
     }
 }
 
-if( ! function_exists('formManagerYesNo') ) {
+if (!function_exists ('formManagerYesNo')) {
 
     /**
      * @param string $type - checkBoxList | dropDownList
@@ -147,7 +147,7 @@ if( ! function_exists('formManagerYesNo') ) {
      * @param bool $showYes
      * @return array
      */
-    function formManagerYesNo(string $type = 'dropDownList', string $id, string $trans, int $required = 0, int $requiredVisible = 0, string $tabID = null, bool $showNo = true, bool $showYes = true)
+    function formManagerYesNo (string $type = 'dropDownList', string $id, string $trans, int $required = 0, int $requiredVisible = 0, string $tabID = null, bool $showNo = true, bool $showYes = true)
     {
         $structure = [
             "tabID"           => $tabID,
@@ -159,15 +159,15 @@ if( ! function_exists('formManagerYesNo') ) {
             "options"         => [],
         ];
 
-        if( $showYes && $showNo ) {
-            $structure['options'][] = ['id' => '0', 'label' => trans('HCTranslations::core.checkbox.no')];
-            $structure['options'][] = ['id' => '1', 'label' => trans('HCTranslations::core.checkbox.yes')];
+        if ($showYes && $showNo) {
+            $structure['options'][] = ['id' => '0', 'label' => trans ('HCTranslations::core.checkbox.no')];
+            $structure['options'][] = ['id' => '1', 'label' => trans ('HCTranslations::core.checkbox.yes')];
         } else {
-            if( $showYes ) {
-                $structure['label'] = " ";
+            if ($showYes) {
+                $structure['label']     = " ";
                 $structure['options'][] = ['id' => '1', 'label' => $trans];
-            } else if( $showNo ) {
-                $structure['label'] = " ";
+            } else if ($showNo) {
+                $structure['label']     = " ";
                 $structure['options'][] = ['id' => '0', 'label' => $trans];
             }
         }
@@ -176,7 +176,7 @@ if( ! function_exists('formManagerYesNo') ) {
     }
 }
 
-if( ! function_exists('formManagerCheckBox') ) {
+if (!function_exists ('formManagerCheckBox')) {
 
     /**
      * @param string $id
@@ -186,7 +186,7 @@ if( ! function_exists('formManagerCheckBox') ) {
      * @param string|null $tabID
      * @return array
      */
-    function formManagerCheckBox(string $id, string $trans, int $required = 0, int $requiredVisible = 0, string $tabID = null)
+    function formManagerCheckBox (string $id, string $trans, int $required = 0, int $requiredVisible = 0, string $tabID = null)
     {
         return [
             "type"            => "checkBoxList",
@@ -200,7 +200,7 @@ if( ! function_exists('formManagerCheckBox') ) {
     }
 }
 
-if( ! function_exists('addAllOptionToDropDownList') ) {
+if (!function_exists ('addAllOptionToDropDownList')) {
 
     /**
      * Adding All options to Drop down list
@@ -208,15 +208,15 @@ if( ! function_exists('addAllOptionToDropDownList') ) {
      * @param array $fieldData
      * @return array
      */
-    function addAllOptionToDropDownList(array $fieldData)
+    function addAllOptionToDropDownList (array $fieldData)
     {
-        array_unshift($fieldData['options'], ['id' => '', $fieldData['showNodes'][0] => trans('HCTranslations::core.all')]);
+        array_unshift ($fieldData['options'], ['id' => '', $fieldData['showNodes'][0] => trans ('HCTranslations::core.all')]);
 
         return $fieldData;
     }
 }
 
-if( ! function_exists('createTranslationKey') ) {
+if (!function_exists ('createTranslationKey')) {
     //TODO move to Translations package
     //TODO improve removal of ,/'?[][\ and etc...
     /**
@@ -225,13 +225,13 @@ if( ! function_exists('createTranslationKey') ) {
      * @param string $string
      * @return mixed
      */
-    function createTranslationKey(string $string)
+    function createTranslationKey (string $string)
     {
-        return str_replace(' ', '_', strtolower($string));
+        return str_replace (' ', '_', strtolower ($string));
     }
 }
 
-if( ! function_exists('hcview') ) {
+if (!function_exists ('hcview')) {
     /**
      * Get the evaluated view contents for the given view.
      *
@@ -240,17 +240,17 @@ if( ! function_exists('hcview') ) {
      * @param  array $mergeData
      * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
-    function hcview($view = null, $data = [], $mergeData = [])
+    function hcview ($view = null, $data = [], $mergeData = [])
     {
-        if( ! array_key_exists($view, config('hc.views')) ) {
-            return view($view, $data, $mergeData);
+        if (!array_key_exists ($view, config ('hc.views'))) {
+            return view ($view, $data, $mergeData);
         }
 
-        return view(array_get(config('hc.views'), $view), $data, $mergeData);
+        return view (array_get (config ('hc.views'), $view), $data, $mergeData);
     }
 }
 
-if( ! function_exists('checkActiveMenuItems') ) {
+if (!function_exists ('checkActiveMenuItems')) {
 
     /**
      * Check if menu item has active sub menu element
@@ -259,22 +259,38 @@ if( ! function_exists('checkActiveMenuItems') ) {
      * @param $routeName
      * @return bool
      */
-    function checkActiveMenuItems(array $item, $routeName)
+    function checkActiveMenuItems (array $item, $routeName)
     {
-        if( $item['route'] == $routeName ) {
+        if ($item['route'] == $routeName) {
             return true;
         }
 
-        if( array_key_exists('children', $item) ) {
-            foreach ( $item['children'] as $child ) {
-                $found = checkActiveMenuItems($child, $routeName);
+        if (array_key_exists ('children', $item)) {
+            foreach ($item['children'] as $child) {
+                $found = checkActiveMenuItems ($child, $routeName);
 
-                if( $found ) {
+                if ($found) {
                     return true;
                 }
             }
         }
 
         return false;
+    }
+}
+
+if (!function_exists ('hcSuccess')) {
+
+    /**
+     * After successful database interaction return success statement
+     * @param string $message
+     * @return array
+     */
+    function hcSuccess (string $message = '')
+    {
+        return [
+            "success" => true,
+            "message" => $message,
+        ];
     }
 }
