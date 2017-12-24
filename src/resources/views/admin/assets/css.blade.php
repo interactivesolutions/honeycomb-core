33669b5848368fc50a0fc85772ba97022fedf53c
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-12-24 12:38:55 +0200

    updated js and css blade for npm

--- src/resources/views/admin/assets/css.blade.php
+++ src/resources/views/admin/assets/css.blade.php
@@ -1,4 +1,14 @@
-{{-- global --}}
+<link rel="stylesheet" href="/css/hc-admin-panel.css">
+
+
+
+
+
+
+
+
+
+{{-- global --}}{{--
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -7,7 +17,4 @@
 <link rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.bootstrap3.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/AdminLTE.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/css/skins/_all-skins.min.css">
-
-{{-- honeycomb--}}
-<link rel="stylesheet" href="/honeycomb/css/honeycomb.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/css/skins/_all-skins.min.css">--}}
